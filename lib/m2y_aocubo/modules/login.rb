6aa3a02dc0ece845e84a34601fd08b50a2068b3d
Caio Lopes <caiotuxo@gmail.com>
2020-09-23 19:07:12 -0300

    signin

---
+++ lib/m2y_aocubo/modules/login.rb
@@ -0,0 +1,18 @@
+module M2yAocubo
+  class Login < Base
+
+    def self.getToken (body)
+      url = "#{baseUrl}/#{TOKEN}"
+      getUrl(url, body)
+    end
+
+
+    def self.signin (username, password)
+      token = getToken.parsed_response["token"]
+      body = {username: username, password: password, signin_token: token}
+      url = "#{baseUrl}/#{SIGNIN}"
+      postUrl(url, body)
+    end
+    
+  end
+end
