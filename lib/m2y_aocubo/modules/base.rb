ca3750fc037b208b5155e34a0e3f9a34f99d0540
Caio Lopes <caiotuxo@gmail.com>
2020-09-23 10:25:51 -0300

    remove phone number

---
+++ lib/m2y_aocubo/modules/base.rb
@@ -0,0 +1,50 @@
+module M2yAocubo
+
+  class Base
+
+    def self.baseUrl
+      M2yAocubo.configuration.ao3_sso_server_url
+    end
+
+    def self.baseHeaders(crypt_id = nil)
+      headers = {'Content-Type' => "application/json"}
+      headers["x-api-key"] = M2yAocubo.configuration.ao3_api_key
+      headers["x-csrf-token"] = "ao#{Time.now.to_i}"
+      if !crypt_id.nil?
+        headers["x-crypto-context"] = crypt_id
+      end
+      headers
+    end
+
+    def self.postUrl(url, body)
+      crypt = cryptContext
+      if crypt.client_public_key.nil?
+        {status: 500}
+      else
+        HTTParty.post(url, headers: basicHeaders(crypt.id), safeBody(body, crypt.client_public_key))
+      end
+    end
+
+
+    def self.cryptContext
+      url = "#{baseUrl}/#{CRYPT_CONTEXT}"
+      req = HTTParty.get(url, headers: basicHeaders)
+      OpenStruct.new(req.parsed_response)
+    end
+
+    def self.safeBody(body, public_key)
+      body[:client_id] = M2yAocubo.configuration.ao3_client_id
+      body.keys.each do |key|
+        if SAFE_KEYS.include?(key)
+          body[key] = crypt(body[key], public_key)
+        end
+      end
+      body.to_json
+    end
+
+    def crypt(string, public_key)
+      Base64.strict_encode64(public_key.public_encrypt(string))
+    end
+
+  end
+end
---
+++ lib/m2y_aocubo/modules/registration.rb
@@ -0,0 +1,10 @@
+module M2yAocubo
+  class Registration < Base
+
+    def self.signup (body)
+      url = "#{baseUrl}/#{SIGNUP}"
+      postUrl(url, body)
+    end
+    
+  end
+end
