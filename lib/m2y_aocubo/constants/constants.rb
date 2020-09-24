02a46c75f85d2aea891726c0b37c27693cf39bca
Caio Lopes <caiotuxo@gmail.com>
2020-09-24 14:52:44 -0300

    password

--- lib/m2y_aocubo/constants/constants.rb
+++ lib/m2y_aocubo/constants/constants.rb
@@ -6,9 +6,9 @@ module M2yAocubo
   SIGNUP = 'hypercube_usm/v1/oauth/signup'
   TOKEN = 'hypercube_usm/v1/oauth/signin/token'
   SIGNIN = 'hypercube_usm/v1/oauth/signin'
-  FORGOT_PASS = 'hypercube_usm​/v1​/oauth​/forgotpassword'
-  CONFIRM_PASS = 'hypercube_usm​/v1​/oauth​/confirmforgotpassword'
   UPDATE_PASS = 'hypercube_usm/v1/oauth/changepassword'
+  CONFIRM_PASS = 'hypercube_usm/v1/oauth/confirmforgotpassword'
+  FORGOT_PASS = 'hypercube_usm/v1/oauth/forgotpassword'
 
   SAFE_KEYS = %w[name username password confirm_password email previousPassword proposedPassword confirmPassword proposedPasswordConfirmation]
   RESTRICTED_KEYS = %w[password itoken password_confirmation]
