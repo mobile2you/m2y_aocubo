cd83aedc4756ee3f1716a543a8430891fd37d466
Caio Lopes <caiotuxo@gmail.com>
2020-09-24 13:44:07 -0300

    password

--- lib/m2y_aocubo/constants/constants.rb
+++ lib/m2y_aocubo/constants/constants.rb
@@ -1,15 +1,15 @@
 module M2yAocubo
-  DYNAMIC_INGEST = "hypercube_dataingest/v1/track/dynamic"
-  IDENTIFY = "hypercube_dataingest/v1/identify"
-  TRACK = "hypercube_dataingest/v1/track"
-  CRYPT_CONTEXT = "hypercube_usm/v1/cryptocontext"
-  SIGNUP = "hypercube_usm/v1/oauth/signup"
-  TOKEN = "hypercube_usm/v1/oauth/signin/token"
-  SIGNIN = "hypercube_usm/v1/oauth/signin"
-  FORGOT_PASS = "hypercube_usm​/v1​/oauth​/forgotpassword"
-  CONFIRM_PASS = "hypercube_usm​/v1​/oauth​/confirmforgotpassword"
-  UPDATE_PASS = "hypercube_usm/v1/oauth/changepassword"
+  DYNAMIC_INGEST = 'hypercube_dataingest/v1/track/dynamic'
+  IDENTIFY = 'hypercube_dataingest/v1/identify'
+  TRACK = 'hypercube_dataingest/v1/track'
+  CRYPT_CONTEXT = 'hypercube_usm/v1/cryptocontext'
+  SIGNUP = 'hypercube_usm/v1/oauth/signup'
+  TOKEN = 'hypercube_usm/v1/oauth/signin/token'
+  SIGNIN = 'hypercube_usm/v1/oauth/signin'
+  FORGOT_PASS = 'hypercube_usm​/v1​/oauth​/forgotpassword'
+  CONFIRM_PASS = 'hypercube_usm​/v1​/oauth​/confirmforgotpassword'
+  UPDATE_PASS = 'hypercube_usm/v1/oauth/changepassword'
 
-  SAFE_KEYS = %w[name username password confirm_password email]
+  SAFE_KEYS = ['name', 'username', 'password', 'confirm_password', 'email']
   RESTRICTED_KEYS = %w[password itoken password_confirmation]
 end
--- lib/m2y_aocubo/version.rb
+++ lib/m2y_aocubo/version.rb
@@ -1,3 +1,3 @@
 module M2yAocubo
-  VERSION = "0.2.7"
+  VERSION = "0.2.8"
 end
